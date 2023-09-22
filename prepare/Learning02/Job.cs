05b3dcfc3ed8a0682d833e53ea5acf85c1ff53a2
JimTheScientist <aidannpratt@gmail.com>
2023-09-21 21:05:41 -0600

    completed Learning02

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+﻿namespace Learning02;
+
+public class Job
+{
+    public string _company = "";// set to empty string to prevent null errors because no constructor :)
+    public string _jobTitle = "";// set to empty string to prevent null errors because no constructor :)
+    public string _startYear = "";// set to empty string to prevent null errors because no constructor :)
+    public string _endYear = "";// set to empty string to prevent null errors because no constructor :)
+    
+
+    public void Display()
+    {
+        System.Console.WriteLine(this._jobTitle + " (" + this._company + ") " + this._startYear + "-" + this._endYear);
+    }
+}
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+﻿namespace Learning02;
+
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs;
+
+    public void Display()
+    {
+        System.Console.WriteLine("Name: " + this._name);
+        System.Console.WriteLine("Jobs:");
+        foreach (Job job in this._jobs)
+        {
+            job.Display();
+        }
+    }
+}
