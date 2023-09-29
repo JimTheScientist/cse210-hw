3a32db0a85b9cbe028d259793b81fa75f6464c3d
JimTheScientist <aidannpratt@gmail.com>
2023-09-28 20:27:42 -0600

    Completed Learning03

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,50 @@
+﻿namespace Learning03;
+
+public class Fraction
+{
+    private int _top = 1;
+    private int _bottom = 1;
+    public Fraction() { } // the things above it set top and bottom to slim down code size :)
+
+    public Fraction(int topNumber, int bottomNumber)
+    {
+        this._top = topNumber;
+        this._bottom = bottomNumber;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        this._top = wholeNumber;
+    }
+
+    public int GetTop()
+    {
+        return this._top;
+    }
+
+    public void SetTop(int top)
+    {
+        this._top = top;
+    }
+
+    public int GetBottom()
+    {
+        return this._bottom;
+    }
+
+    public void GetBottom(int bottom)
+    {
+        this._bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return this._top.ToString() + "/" + this._bottom;
+    }
+
+    public double GetDecimalValue()
+    {
+        return ((double)this._top) / ((double)this._bottom);
+    }
+    
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,10 @@
 using System;
+using Learning03;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine((new Fraction(1,3).GetDecimalValue()));
     }
 }
