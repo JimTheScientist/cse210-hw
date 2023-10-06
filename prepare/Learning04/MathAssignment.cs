387da93d20bb5a0a91beec66759791e35263418f
JimTheScientist <aidannpratt@gmail.com>
2023-10-05 21:18:55 -0600

    completed Learning04.csproj

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,18 @@
+﻿namespace Learning04;
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string topic, string studentName, string textbookSection, string problems) : base(studentName, topic)
+    {
+        this._textbookSection = textbookSection;
+        this._problems = problems;
+    }
+    
+    public string GetHomeworkList()
+    {
+        return "Section " + this._textbookSection + " Problems " + this._problems;
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using Learning04;
+using Learning04.obj;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        WritingAssignment writingAssignment = new WritingAssignment("European History", "Mary Waters",
+            "The Causes of World War II");
+        MathAssignment mathAssignment = new MathAssignment("Fractions", "Roberto Rodriguez", "7.3", "8-19");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine(assignment.GetSummary());
     }
 }
