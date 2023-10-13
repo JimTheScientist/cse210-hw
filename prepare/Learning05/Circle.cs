7a573d3799b8f4120957e97a6faf369d1774e980
JimTheScientist <aidannpratt@gmail.com>
2023-10-12 23:12:04 -0600

    Learning05 complete

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,15 @@
+﻿namespace Learning05;
+
+public class Circle : Shape
+{
+    public double _radius;
+    public Circle(string color, double radius) : base(color)
+    {
+        this._radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * (_radius * _radius);
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using Learning05;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("Testing square, rectangle, and circle");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("red", 5));
+        shapes.Add(new Rectangle("green", 3,4));
+        shapes.Add(new Circle("blue", 2));
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+        }
     }
 }
